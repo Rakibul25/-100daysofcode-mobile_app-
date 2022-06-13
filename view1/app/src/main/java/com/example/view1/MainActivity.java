76a143d23b666f4ba5e7895129faeb35cc574532
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-06-13 18:16:27 +0600

    simple list

--- view1/app/src/main/java/com/example/view1/MainActivity.java
+++ view1/app/src/main/java/com/example/view1/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.view1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+    // Array of strings...
+    ListView simpleList;
+    String arlist[] = {"Rakib", "Rakibul", "Rakibul", "Rakibul", "Rakibul", "Rakibul"};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @Override   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_main);
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, arlist);
+        simpleList.setAdapter(arrayAdapter);
     }
 }
