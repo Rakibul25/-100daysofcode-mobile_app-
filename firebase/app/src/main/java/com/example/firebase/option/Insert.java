8e0a308876b617da145b51f47aec01ebdc8f1f4b
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-07-03 23:55:49 +0600

    day 11 done

---
+++ firebase/app/src/main/java/com/example/firebase/option/Insert.java
@@ -0,0 +1,16 @@
+package com.example.firebase.option;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firebase.R;
+
+public class Insert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
