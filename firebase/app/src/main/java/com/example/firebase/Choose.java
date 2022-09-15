2bd58e324caefb95de9b1b397e17c0ae0b6e2536
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-09-16 02:26:54 +0600

    logic for intent

--- firebase/app/src/main/java/com/example/firebase/Choose.java
+++ firebase/app/src/main/java/com/example/firebase/Choose.java
@@ -2,8 +2,12 @@ package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.example.firebase.option.Insert;
 
 public class Choose extends AppCompatActivity {
     Button btnInsert;
@@ -15,5 +19,14 @@ public class Choose extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        btnInsert = findViewById(R.id.btnInsert);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Insert.class);
+                startActivity(myIntent);
+            }
+
+        });
     }
 }
--- firebase/app/src/main/java/com/example/firebase/option/Insert.java
+++ firebase/app/src/main/java/com/example/firebase/option/Insert.java
@@ -2,16 +2,32 @@ package com.example.firebase.option;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.firebase.R;
 
 public class Insert extends AppCompatActivity {
-
+    EditText name,id;
+    Button insertbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
-        //tst
+        name = findViewById(R.id.name);
+        id = findViewById(R.id.id);
+        insertbutton = findViewById(R.id.btnInsert);
+
+        insertbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Insert.class);
+                startActivity(myIntent);
+            }
+
+        });
+
     }
 }
