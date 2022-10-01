f7b60fac6d1393991afb344644a01a9b6df97fd5
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-10-01 16:19:55 +0600

    insert done

---
+++ firebase/app/src/main/java/com/example/firebase/Dobject.java
@@ -0,0 +1,17 @@
+package com.example.firebase;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Dobject {
+    private DatabaseReference databaseReference;
+    public Dobject(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        databaseReference = db.getReference(User.class.getSimpleName());
+    }
+    public Task<Void> add(User user){
+        return databaseReference.push().setValue(user);
+    }
+
+}
--- firebase/app/src/main/java/com/example/firebase/option/Insert.java
+++ firebase/app/src/main/java/com/example/firebase/option/Insert.java
@@ -7,22 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.firebase.Dobject;
 import com.example.firebase.R;
+import com.example.firebase.User;
 
 public class Insert extends AppCompatActivity {
-    EditText name,id;
     Button insertbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
-        name = findViewById(R.id.name);
-        id = findViewById(R.id.id);
+        final EditText name = findViewById(R.id.name);
+        final EditText id = findViewById(R.id.id);
         insertbutton = findViewById(R.id.btnInsert);
-
+        Dobject dobject = new Dobject();
         insertbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                User user = new User(name.getText().toString(),id.getText().toString());
+                dobject.add(user).addOnSuccessListener(suc ->{
+                    Toast.makeText(Insert.this, "added", Toast.LENGTH_SHORT).show();
+                }).addOnCompleteListener(er ->
+                {
+                    Toast.makeText(Insert.this, "failed", Toast.LENGTH_SHORT).show();
+                });
 
             }
 
