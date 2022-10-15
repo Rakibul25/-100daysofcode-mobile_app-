cdf604f4e4ac2ff98a398a78bb982612db94bcdc
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-10-15 23:52:54 +0600

    basic app update

--- tut/lib/main.dart
+++ tut/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,13 +30,57 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         children: <Widget>[
           Container(
-            height: size.height*.3,
-            decoration: BoxDecoration(
+            height: size.height*.5,
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 alignment: Alignment.topCenter,
                 image: AssetImage('assets/images/top.png')
               ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 64,
+                    child: Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage('https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                        ),
+                        SizedBox(width: 16,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Person one',style: TextStyle(color: Colors.white,fontSize: 20)),
+                            Text('2342342342',style: TextStyle(color: Colors.white,fontSize: 15))
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          GridView.count(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            primary: false,
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    SvgPicture.network('https://freesvg.org/img/BenBois_Graduation.png')
+                  ],
+                ),
+              )
+            ],
+            crossAxisCount: 2,
           )
         ],
       ),
