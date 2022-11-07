1c99870713426cf8857d21069091db8b2dac7ce3
rakib25 <mislam171229@bscse.uiu.ac.bd>
2022-11-07 12:41:35 +0600

    user

--- bank/lib/Screens/home_screen.dart
+++ bank/lib/Screens/home_screen.dart
@@ -15,12 +15,28 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         margin: EdgeInsets.only(top: 8),
         child: ListView(
+          physics: const ClampingScrollPhysics(),
           children: <Widget>[
             Container(
-              margin: EdgeInsets.only(left: 16, right: 16),
+              margin: EdgeInsets.only(left: 16, right: 16, top: 16),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  SvgPicture.asset('assets/svg/navbar.svg')
+                  GestureDetector(
+                    onTap: (){
+                      print('tapped');
+                    },
+                      child: SvgPicture.asset('assets/svg/navbar.svg')),
+                  Container(
+                    height: 59,
+                    width: 59,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/user.jpg')
+                      )
+                    ),
+                  )
                 ],
               ),
             )
