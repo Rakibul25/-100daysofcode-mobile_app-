cb5ee19b89561af1b40f829514b80edddbd39a8c
rakib25 <mislam171229@bscse.uiu.ac.bd>
2023-01-15 17:31:27 +0600

    home page ready

---
+++ homerent/lib/data/sample_data.dart
@@ -0,0 +1,22 @@
+const house_Data = [
+  {
+    "image" : "assets/images/image1.jpg",
+    "amount" : 20000,
+    "address" : "Dhaka",
+    "bedrooms" : 4,
+    "bathrooms" : 2,
+    "area" : 1000,
+    "garage" : 1,
+    "description" : "helloooooo"
+  },
+  {
+    "image" : "assets/images/image2.jpg",
+    "amount" : 20000,
+    "address" : "Dhaka",
+    "bedrooms" : 4,
+    "bathrooms" : 2,
+    "area" : 1000,
+    "garage" : 1,
+    "description" : "helloooooo"
+  }
+];
