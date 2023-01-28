dac0779de0fc439dadc04c85bd46b841960fb4f1
rakib25 <mislam171229@bscse.uiu.ac.bd>
2023-01-29 02:11:42 +0600

    button added

--- rakib/lib/screen/pc_screen/home_pc.dart
+++ rakib/lib/screen/pc_screen/home_pc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rakib/constants/color_constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePc extends StatefulWidget {
   const HomePc({Key? key}) : super(key: key);
@@ -19,7 +20,7 @@ class _HomePcState extends State<HomePc> {
         child: Container(
           color: COLOR_BLACK,
           child: Padding(
-            padding: const EdgeInsets.only(left: 20,right: 20),
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
             child: Row(
               children: [
                 ClipRRect(
@@ -34,86 +35,74 @@ class _HomePcState extends State<HomePc> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      GestureDetector(
-                        child: InkWell(
-                          onTap: (){
-                            setState(() {
-                              isHovering = true;
-                            });
-                          },
-
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                isHovering.toString(),
-                                style: TextStyle(
-                                  color: isHovering
-                                      ? Colors.blue.shade200
-                                      : Colors.white,
-                                ),
-                              ),
-                              SizedBox(height: 5),
-                              // For showing an underline on hover
-                              Visibility(
-                                maintainAnimation: true,
-                                maintainState: true,
-                                maintainSize: true,
-                                visible: isHovering,
-                                child: Container(
-                                  height: 2,
-                                  width: 20,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              ],
-                          ),
-                        ),
-                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          print('Download CV!');
+                          const url = 'https://flutterdevs.com/';
+                          if (await canLaunch(url)) {
+                          await launch(url, forceSafariVC: true, forceWebView: true);
+                          } else {
+                          throw 'Could not launch $url';
+                          }
+                        },
+                        child: const Text('Download CV'),
+                        style: ElevatedButton.styleFrom(
+                            shape: const StadiumBorder(),
+                            minimumSize: const Size(200, 50),
+                            primary: Colors.teal),
+                      )
                     ],
                   ),
                 ),
                 SizedBox(
                   width: width / 50,
                 ),
-
               ],
             ),
           ),
         ),
       ),
-
       body: Container(
           color: COLOR_BLACK,
           child: Row(
             children: [
               Container(
-                width: width*0.55,
+                width: width * 0.55,
                 child: Padding(
                   padding: const EdgeInsets.all(30.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Hi there!", style: TextStyle(fontSize: 20,color: Colors.white),)
+                      Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: Image.asset(
+                            'assets/images/myavatar.png',
+                            height: 350,
+                            width: 350,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
               Container(
-                width: width*0.45,
+                width: width * 0.45,
                 child: Padding(
                   padding: const EdgeInsets.all(30.0),
                   child: Column(
                     children: [
-                      Text("2nd", style: TextStyle(fontSize: 20,color: Colors.white),)
+                      Text(
+                        "2nd",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      )
                     ],
                   ),
                 ),
               )
             ],
-          )
-
-      ),
+          )),
     );
   }
 }
