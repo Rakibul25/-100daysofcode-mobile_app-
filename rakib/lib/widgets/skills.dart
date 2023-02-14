6953721e353a0cf81dd321406d06f021b9f762dc
rakib25 <mislam171229@bscse.uiu.ac.bd>
2023-02-15 02:57:38 +0600

    circle done

--- rakib/lib/widgets/skills.dart
+++ rakib/lib/widgets/skills.dart
@@ -11,9 +11,7 @@ class Skills extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery
-        .of(context)
-        .size;
+    var size = MediaQuery.of(context).size;
     //CircularProgressIndicator(value: 0.6,);
     return Container(
         width: size.width,
@@ -28,77 +26,55 @@ class Skills extends StatelessWidget {
                   return Container(
                     width: size.width * .65,
                     child: Column(
-                        children: [
+                      children: [
                         Text("Skills",
-                        style: GoogleFonts.getFont('Delius',
-                            color: CustomColors.primary, fontSize: 20)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                      Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Lottie.asset('assets/lotties/programming.json',
-                            height: 200, width: 200),
-                        Text("Language & Tools",
                             style: GoogleFonts.getFont('Delius',
-                                color: CustomColors.gray,
-                                fontSize: 22)),
+                                color: CustomColors.primary, fontSize: 20)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Lottie.asset('assets/lotties/programming.json',
+                                    height: 200, width: 200),
+                                Text("Language & Tools",
+                                    style: GoogleFonts.getFont('Delius',
+                                        color: CustomColors.gray,
+                                        fontSize: 22)),
+                              ],
+                            ),
+                            const SkillCard(percentage: 0.7, label: "Flutter")
+                          ],
+                        ),
+                        SizedBox(width: 0.05 * size.width),
+                        // hello with bio and info
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [],
+                        ),
                       ],
                     ),
-                        SizedBox(
-                          height: 120,
-                          width: 120,
-                          child: new CircularPercentIndicator(
-                            radius: 50.0,
-                            lineWidth: 8.0,
-                            animation: true,
-                            percent: 0.7,
-                            center: new Text(
-                              "70.0%",
-                              style:
-                              new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                            ),
-                            footer: new Text(
-                              "Sales this week",
-                              style:
-                              new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-                            ),
-                            circularStrokeCap: CircularStrokeCap.round,
-                            progressColor: Colors.purple,
-                          ),
-                        ),
-
-                ],
-                  ),
-                SizedBox(width: 0.05 * size.width),
-                // hello with bio and info
-                Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
-                ),
-                ],
-                ),
-                );
+                  );
                 } else {
-                return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                // skills cards
-                Text("Skills",
-                style: GoogleFonts.getFont('Delius',
-                color: CustomColors.primary, fontSize: 20)),
-                const SizedBox(height: 10),
-                Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
-                ),
-                // hello with bio and info
-                ],
-                );
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // skills cards
+                      Text("Skills",
+                          style: GoogleFonts.getFont('Delius',
+                              color: CustomColors.primary, fontSize: 20)),
+                      const SizedBox(height: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [],
+                      ),
+                      // hello with bio and info
+                    ],
+                  );
                 }
               }),
             ]));
--- rakib/lib/widgets/skillscard.dart
+++ rakib/lib/widgets/skillscard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SkillsCard extends StatelessWidget {
-  const SkillsCard({
+class SkillCard extends StatelessWidget {
+  const SkillCard({
     Key? key,
     required this.percentage,
     required this.label,
@@ -12,36 +12,45 @@ class SkillsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TweenAnimationBuilder(
-          tween: Tween<double>(begin: 0, end: percentage),
-          duration: Duration(seconds: 1),
-          builder: (context, double value, child) => Stack(
-            fit: StackFit.expand,
-            children: [
-              CircularProgressIndicator(
-                value: value,
-                color: Colors.black,
-                backgroundColor: Colors.white,
-              ),
-              Center(
-                child: Text(
-                  (value * 100).toInt().toString() + "%",
-                  style: Theme.of(context).textTheme.subtitle1,
+    return SizedBox(
+      height: 100,
+      width: 100,
+      child: Column(
+        children: [
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: TweenAnimationBuilder(
+                tween: Tween<double>(begin: 0, end: percentage),
+                duration: Duration(seconds: 4),
+                builder: (context, double value, child) => Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    CircularProgressIndicator(
+                      value: value,
+                      color: Colors.brown,
+                      backgroundColor: Colors.white,
+                    ),
+                    Center(
+                      child: Text(
+                        (value * 100).toInt().toString() + "%",
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          label,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.subtitle2,
-        ),
-      ],
+          SizedBox(height: 20),
+          Text(
+            label,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ],
+      ),
     );
   }
 }
